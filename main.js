881903eab461452617dba49e1e62705b7f3f3905
sawich <sawich@angeldev.org>
2018-09-11 03:36:49 +0300

    fst

---
+++ main.js
@@ -0,0 +1,142 @@
+let lFollowX = 0;
+let x = 0;
+
+const friction = 0.8 / 60;
+const el = document.querySelector('.pars');
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  
+  el.style['transform'] = `translateX(${x}vw)`;
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+function mouse_animation (e) {
+  lFollowX = -((e.pageX + el.offsetLeft) / 120);
+}
+
+function main() {
+  particlesJS("particles-js", 
+  {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#f7b8fe"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#f453fd",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 800,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+  });
+
+  const particles = document.querySelector("#particles-js");
+  particles.classList.add("particles-js-load-start");
+
+  //const par = document.querySelector('.par');
+  //par.classList.add('par-load-start');
+}
+
+//window.onmousemove = mouse_animation;
+window.onload = main;
+
+//moveBackground();
